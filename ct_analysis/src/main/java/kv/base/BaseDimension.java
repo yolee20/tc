708ee3a38494daad0ea9a59b95248bb1831351a3
yolee <zhangyaolin910920@163.com>
2019-05-28 20:28:48 +0800

    first commit on 2019.5.28

---
+++ ct_analysis/src/main/java/kv/base/BaseDimension.java
@@ -0,0 +1,15 @@
+package kv.base;
+
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public abstract class BaseDimension implements WritableComparable<BaseDimension>{
+    public abstract int compareTo(BaseDimension o);
+
+    public abstract void write(DataOutput out) throws IOException;
+
+    public abstract void readFields(DataInput in) throws IOException;
+}
